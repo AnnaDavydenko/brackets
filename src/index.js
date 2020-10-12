9e08c8ff53329b0e78f0ae8a010638f05d7bd951
AnnaDavydenko <annadavydenko2544737@gmail.com>
2020-10-12 22:55:00 +0300

    brackets all tests

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
     var res = true;
-    //console.log(`str = ${str}`);
     var cicleStr = str;
     for (var i = 0; i < bracketsConfig.length; i++) {
         var open = bracketsConfig[i][0];
         var close = bracketsConfig[i][1];
         var strCopy = removeBrackets(cicleStr, open, close);
-        //console.log(`strCopy = ${strCopy}`);
-        //console.log(i);
-        if (str === "") {
+        if (strCopy === "") {
             res = true;
         } else if ((i === bracketsConfig.length - 1) && str === cicleStr) {
             res = false;
