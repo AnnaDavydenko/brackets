f7a69d68052cc2fc117f3b96734f8a6634a6f75e
AnnaDavydenko <annadavydenko2544737@gmail.com>
2020-10-12 22:48:38 +0300

    brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    var res = true;
+    //console.log(`str = ${str}`);
+    var cicleStr = str;
+    for (var i = 0; i < bracketsConfig.length; i++) {
+        var open = bracketsConfig[i][0];
+        var close = bracketsConfig[i][1];
+        var strCopy = removeBrackets(cicleStr, open, close);
+        //console.log(`strCopy = ${strCopy}`);
+        //console.log(i);
+        if (str === "") {
+            res = true;
+        } else if ((i === bracketsConfig.length - 1) && str === cicleStr) {
+            res = false;
+        } else if ((i === bracketsConfig.length - 1) && str !== cicleStr) {
+            i = -1;
+            str = cicleStr;
+        }
+        cicleStr = strCopy;
+    }
+    return res;
+}
+
+function removeBrackets(str, open, close) {
+    let result = str;
+    let hasPairs = str.includes(`${open}${close}`);
+    while (hasPairs) {
+        result = result.replace(`${open}${close}`, '');
+        hasPairs = result.includes(`${open}${close}`);
+    }
+    return result;
 }
